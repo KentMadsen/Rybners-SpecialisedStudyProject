dd577c367d84efe5f2c5f9d6cdbe5f9b274ae226
Kent vejrup Madsen <Blindedhollow@gmail.com>
2016-11-28 02:37:56 +0100

    up

---
+++ SRP/Razor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Razor
+{
+    class Program
+    {
+
+        static void Main( string[] args )
+        {
+
+            bool Continue = false;
+
+            while( Continue )
+            {
+                string Command = Console.ReadLine();
+
+            }
+
+        } // End Main
+
+    } // Program
+
+} // End Namespace
